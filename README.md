fff9b8a9bab4ca995d6cd8344e203196dde16590
Eric CHASSERIAUD <eric.chasseriaud@gmail.com>
2020-05-06 15:32:32 +0000

    fist commit of action

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # bump-version-bash
+
+# Github Action to generate a project version from file.
+
+# with bash only
---
+++ main.sh
@@ -0,0 +1,52 @@
+#!/bin/bash -l
+
+set -euo pipefail
+
+main() {
+
+  local path
+  local result
+  local step
+  path="$1"
+  step="$2"
+
+  echo "path to version-file: $path"
+  result=$(cat $path)
+  echo "current version: $result"
+  
+  if [ $result != "first" ]
+  then
+  
+    RE='[^0-9]*\([0-9]*\)[.]\([0-9]*\)[.]\([0-9]*\)\([0-9A-Za-z-]*\)'
+
+    MAJOR=`echo $result | sed -e "s#$RE#\1#"`
+    MINOR=`echo $result | sed -e "s#$RE#\2#"`
+    PATCH=`echo $result | sed -e "s#$RE#\3#"`
+
+    case "$step" in
+      major)
+        let MAJOR+=1
+        ;;
+      minor)
+        let MINOR+=1
+        ;;
+      patch)
+        let PATCH+=1
+        ;;
+    esac
+  
+  else
+
+    echo "that is the first build !!!"
+    MAJOR=1
+    MINOR=0
+    PATCH=0
+    
+  fi
+
+  echo "new version: $MAJOR.$MINOR.$PATCH"
+
+  echo ::set-output name=value::"$MAJOR.$MINOR.$PATCH"
+}
+
+main "$1" "$2"
